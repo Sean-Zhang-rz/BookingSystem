613984fcdaef742def2d72b11cdc3e9d161f1e10
Sean <770899447@qq.com>
2024-06-28 22:25:07 +0800

    booking

---
+++ src/booking/booking.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookingDto } from './dto/create-booking.dto';
+import { UpdateBookingDto } from './dto/update-booking.dto';
+import { Booking } from './entities/booking.entity';
+import { Between, EntityManager, Like } from 'typeorm';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { MeetingRoom } from 'src/meeting-room/entities/meeting-room.entity';
+
+@Injectable()
+export class BookingService {
+  @InjectEntityManager()
+  private entityManager: EntityManager;
+
+  async initData() {
+    const user1 = await this.entityManager.findOneBy(User, {
+      id: 1,
+    });
+    const user2 = await this.entityManager.findOneBy(User, {
+      id: 2,
+    });
+
+    const room1 = await this.entityManager.findOneBy(MeetingRoom, {
+      id: 3,
+    });
+    const room2 = await await this.entityManager.findOneBy(MeetingRoom, {
+      id: 6,
+    });
+
+    const booking1 = new Booking();
+    booking1.room = room1;
+    booking1.user = user1;
+    booking1.startTime = new Date();
+    booking1.endTime = new Date(Date.now() + 1000 * 60 * 60);
+
+    await this.entityManager.save(Booking, booking1);
+
+    const booking2 = new Booking();
+    booking2.room = room2;
+    booking2.user = user2;
+    booking2.startTime = new Date();
+    booking2.endTime = new Date(Date.now() + 1000 * 60 * 60);
+
+    await this.entityManager.save(Booking, booking2);
+
+    const booking3 = new Booking();
+    booking3.room = room1;
+    booking3.user = user2;
+    booking3.startTime = new Date();
+    booking3.endTime = new Date(Date.now() + 1000 * 60 * 60);
+
+    await this.entityManager.save(Booking, booking3);
+
+    const booking4 = new Booking();
+    booking4.room = room2;
+    booking4.user = user1;
+    booking4.startTime = new Date();
+    booking4.endTime = new Date(Date.now() + 1000 * 60 * 60);
+
+    await this.entityManager.save(Booking, booking4);
+  }
+
+  async find(
+    pageNo: number,
+    pageSize: number,
+    username: string,
+    meetingRoomName: string,
+    meetingRoomPosition: string,
+    bookingTimeRangeStart: number,
+    bookingTimeRangeEnd: number,
+  ) {
+    const skipCount = (pageNo - 1) * pageSize;
+
+    const [bookings, totalCount] = await this.entityManager.findAndCount(
+      Booking,
+      {
+        where: {
+          user: {
+            username: Like(`%${username}%`),
+          },
+          room: {
+            name: Like(`%${meetingRoomName}%`),
+            location: Like(`%${meetingRoomPosition}%`),
+          },
+          startTime: Between(
+            new Date(bookingTimeRangeStart),
+            new Date(bookingTimeRangeEnd),
+          ),
+        },
+        relations: {
+          user: true,
+          room: true,
+        },
+        skip: skipCount,
+        take: pageSize,
+      },
+    );
+
+    return {
+      bookings,
+      totalCount,
+    };
+  }
+}
